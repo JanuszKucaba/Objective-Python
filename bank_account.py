54070ad022a4ef1c84a83d7be7f2fc4602b328e3
Janusz K <jankuc115@gmail.com>
2022-03-30 23:39:03 +0200

    bank account class

---
+++ bank_account.py
@@ -0,0 +1,34 @@
+'''
+Bank account class
+'''
+
+class BankAccount:
+    '''Takes name balance and bank name
+    could retur info about account, witdrwa and deposit'''
+    bank_name = 'Commercial Bank, Palace Street, Rzeszow'
+
+    def __init__(self, name, balance=0, bank=bank_name):
+        '''initialization class'''
+        self.name = name
+        self.balance = balance
+        self.bank = bank
+
+    def display(self):
+        '''display account info: name, balance and bank name'''
+        print(self.name, self.balance, self.bank)
+
+    def withdraw(self, amount):
+        '''choosing money'''
+        self.balance -= amount
+
+    def deposit(self, amount):
+        '''adding money'''
+        self.balance += amount
+
+if __name__ == '__main__':
+
+    a1 = BankAccount('Johny', 500, 'Central Bank')
+    a2 = BankAccount('Tom')
+
+    a1.display()
+    a2.display()
