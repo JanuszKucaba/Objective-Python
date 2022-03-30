255f83a19b814e48a3b137a0bc3a9b7ba4114c8b
Janusz K <jankuc115@gmail.com>
2022-03-31 00:04:20 +0200

    check class  methods

--- bank_account.py
+++ bank_account.py
@@ -27,9 +27,13 @@ class BankAccount:
 
 if __name__ == '__main__':
     a1 = BankAccount('John', 500, 'Central Bank')
-    a2 = BankAccount('Tom', '100')
+    a2 = BankAccount('Tom', 100)
     a3 = BankAccount('George')
 
     a1.display()
     a2.display()
+    a2.withdraw(50)
     a2.display()
+    a3.display()
+    a3.deposit(500)
+    a3.display()
